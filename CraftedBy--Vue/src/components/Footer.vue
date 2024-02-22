4e2a30021d84bc66472f74036001678f793c33fe
Lucas HOANG <lucas.hoang@le-campus-numerique.fr>
2024-02-22 16:36:54 +0100

    ADD Footer Component

---
+++ CraftedBy--Vue/src/components/Footer.vue
@@ -0,0 +1,82 @@
+<script>
+
+</script>
+
+<template>
+
+    <footer class="font-dm-sans text-lg bg-black text-white">
+        <div class="w-screen border-y-2 py-5">
+            <a href="/" class="link link-hover pl-6">Shop All</a>
+        </div> 
+
+        <div class="w-screen border-b-2 py-5">
+            <a href="/" class="link link-hover pl-6">LEARN</a>
+            <div class="text-base pl-6 pt-3">
+                <ul>
+                    <li>
+                        <a href="/" class="link link-hover">Our Story</a>
+                    </li>
+                    <li>
+                        <a href="/" class="link link-hover">Our Craftmans</a>
+                    </li>
+                    <li>
+                        <a href="/" class="link link-hover">Partners</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="w-screen border-b-2 py-5">
+            <a href="/" class="link link-hover pl-6">HELP</a>
+            <div class="text-base pl-6 pt-3">
+                <ul>
+                    <li>
+                        <a href="/" class="link link-hover">Account</a>
+                    </li>
+                    <li>
+                        <a href="/" class="link link-hover">Wholesale</a>
+                    </li>
+                    <li>
+                        <a href="/" class="link link-hover">FAQs</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="w-screen border-b-2 py-5">
+            <a href="/" class="link link-hover pl-6">FOLLOW</a>
+            <div class="text-base pl-6 pt-3">
+                <ul>
+                    <li>
+                        <a href="/" class="link link-hover">Instagram</a>
+                    </li>
+                    <li>
+                        <a href="/" class="link link-hover">Twitter</a>
+                    </li>
+                    <li>
+                        <a href="/" class="link link-hover">TikTok</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="w-screen py-5">
+            <a href="/" class="link link-hover pl-6">LEGAL</a>
+            <div class="text-base pl-6 pt-3">
+                <ul>
+                    <li>
+                        <a href="/" class="link link-hover">Terms of use</a>
+                    </li>
+                    <li>
+                        <a href="/" class="link link-hover">Privacy policy</a>
+                    </li>
+                    <li>
+                        <a href="/" class="link link-hover">Cookie policy</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+    </footer>
+
+</template>
