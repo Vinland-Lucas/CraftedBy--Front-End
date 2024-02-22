6aa6fd991e965120693580d94da2f96b2b8d105f
Lucas HOANG <lucas.hoang@le-campus-numerique.fr>
2024-02-22 16:36:45 +0100

    ADD Header Component

---
+++ CraftedBy--Vue/src/components/Header.vue
@@ -0,0 +1,35 @@
+<script>
+
+</script>
+
+<template>
+
+    <header>
+        <div class="header--container fixed navbar bg-base-100 bg-white text-black border-b border-solid border-black">
+            <div class="flex-none">
+                <button class="btn btn-square btn-ghost">
+                    <i-ph-list style="font-size: 24px; margin-left: 8px;"></i-ph-list>
+                </button>
+            </div>
+            <div class="flex-1">
+                <a class="btn btn-ghost text-xl font-ryman-eco pl-0">
+                    <img src="../assets/craftedbylogo.svg" alt="CraftedBy Logo" width="155"/>
+                </a>
+            </div>
+            <div class="flex-none mr-px">
+                <a class="btn btn-square btn-ghost">
+                    <i-ph-user style="font-size: 24px; "></i-ph-user>
+                </a>
+
+                <button class="btn btn-square btn-ghost">
+                    <i-ph-bag style="font-size: 24px; margin-right: 16px;"></i-ph-bag>
+                </button>
+            </div>
+        </div>
+    </header>
+
+</template>
+
+<style>
+
+</style>
