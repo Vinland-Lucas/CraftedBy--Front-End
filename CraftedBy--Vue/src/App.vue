1582805d1e3eacd818ccb13a250690585eac70a6
Lucas HOANG <lucas.hoang@le-campus-numerique.fr>
2024-02-22 17:05:14 +0100

    ADD Header & Footer to App.vue file

--- CraftedBy--Vue/src/App.vue
+++ CraftedBy--Vue/src/App.vue
@@ -1,9 +1,12 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
 </script>
 
 <template>
+  <!--
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
@@ -16,8 +19,14 @@ import HelloWorld from './components/HelloWorld.vue'
       </nav>
     </div>
   </header>
+  -->
+
+  <Header />
 
   <RouterView />
+
+  <Footer />
+
 </template>
 
 <style scoped>
