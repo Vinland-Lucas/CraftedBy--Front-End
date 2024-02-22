70b7f00f550488305421f1f41a81a4047b714b2f
Lucas HOANG <lucas.hoang@le-campus-numerique.fr>
2024-02-22 17:04:23 +0100

    UPDT Landing Page by adding the bg-image (not finished)

--- CraftedBy--Vue/src/views/HomeView.vue
+++ CraftedBy--Vue/src/views/HomeView.vue
@@ -4,6 +4,21 @@ import TheWelcome from '../components/TheWelcome.vue'
 
 <template>
   <main>
-    <TheWelcome />
+    <div class="main--container">
+      <div class="w-screen h-screen bg-landing-page bg-cover bg-top flex justify-center items-center">
+        <div class="w-5/6 h-3/6 flex flex-col justify-evenly ">
+          <div class="h-2/6 flex flex-col justify-between items-center">
+            <h1 class="font-ryman-eco text-3xl text-black">Welcome to CraftedBy</h1>
+
+            <p class="font-dm-sans text-base text-black text-center">
+              Fostering global artisan empowerment, celebrating unique creations
+            </p>
+          </div>
+          <a class="btn btn-outline">SHOP NOW</a>
+        </div>
+        
+      </div>
+      
+    </div>
   </main>
 </template>
