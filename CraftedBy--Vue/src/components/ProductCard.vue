3c7c6bef80dabd7bb427e41b231e622092be56a1
Vinland-Lucas <lucas.hoang@le-campus-numerique.fr>
2024-02-28 23:35:09 +0100

    UPDT ProductCard by rezising it, resizing the container of product image, adding the picture and make it fit in his container

--- CraftedBy--Vue/src/components/ProductCard.vue
+++ CraftedBy--Vue/src/components/ProductCard.vue
@@ -3,10 +3,18 @@
 </script>
 
 <template>
-    <div class="card w-36 bg-base-100 shadow-xl">
-        <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+    <div class="product-card w-pc-width h-pc-height bg-base-100 shadow-xl mb-3.5">
+        <div class="product-image w-pc-width h-40">
+            <a href="/">
+                <img 
+                    src="../../public/pexels-aidan-nguyen-2735644.webp" alt="Crafted Knife" 
+                    style="width: 100%; height: 100%; object-fit: cover; object-position: center;"
+                />
+            </a>
+        </div>
+
         <div class="card-body">
-            <h2 class="card-title">Shoes!</h2>
+            <h2 class="card-title">VALORANT KNIFE</h2>
             <p>If a dog chews shoes whose shoes does he choose?</p>
             <div class="card-actions justify-end">
             <button class="btn btn-primary">Buy Now</button>
