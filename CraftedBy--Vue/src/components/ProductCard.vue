a53f881f3ddf29a2d7b2ffdd1df31b75c27bd1bd
Vinland-Lucas <lucas.hoang@le-campus-numerique.fr>
2024-03-03 22:19:11 +0100

    UPDT ProductCard component : finished design

--- CraftedBy--Vue/src/components/ProductCard.vue
+++ CraftedBy--Vue/src/components/ProductCard.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-    <div class="product-card w-pc-width h-pc-height bg-base-100 shadow-xl mb-3.5">
+    <div class="product-card w-pc-width h-pc-height bg-transparent shadow-sm mb-3.5 font-dm-sans-light">
         <div class="product-image w-pc-width h-40">
             <a href="/">
                 <img 
@@ -13,12 +13,35 @@
             </a>
         </div>
 
-        <div class="card-body">
-            <h2 class="card-title">VALORANT KNIFE</h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
-            <div class="card-actions justify-end">
-            <button class="btn btn-primary">Buy Now</button>
+        <div class="product-infos py-3 ">
+            <h3 class="text-base font-normal leading-none tracking-tight mb-2.5"><a href="/">VALORANT KNIFE DE ZINZIN #SLASH</a></h3>
+
+            <div>
+                <span class="text-xs leading-none">Color</span>
+
+                <div class="flex gap-1.5 mt-1 mb-2.5">
+                    <button type="button" class="w-7 h-7 bg-slate-700 border-[0.5px] border-white"></button>
+                    <button type="button" class="w-7 h-7 bg-red-700 border-[0.5px] border-white"></button>
+                    <button type="button" class="w-7 h-7 bg-emerald-400 border-[0.5px] border-white"></button>
+                </div>
             </div>
+            
+            <div class="flex justify-between mt-3">
+                <div class="flex">
+                    <i-ph-star style="font-size: 11px;"></i-ph-star>
+                    <i-ph-star style="font-size: 11px;"></i-ph-star>
+                    <i-ph-star style="font-size: 11px;"></i-ph-star>
+                    <i-ph-star style="font-size: 11px;"></i-ph-star>
+                    <i-ph-star style="font-size: 11px;"></i-ph-star>
+                </div>
+
+                <div class="text-[11px]">+ 500 Reviews</div>
+            </div>
+
+        </div>
+
+        <div class="flex justify-center w-full">
+            <button type="button" class="btn btn-active px-8 rounded-none font-normal text-white bg-black">ADD TO CART</button>
         </div>
     </div>
 </template>
