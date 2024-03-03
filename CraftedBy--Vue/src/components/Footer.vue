3c32cb0f485d096192bab9e3d9ea06d6a75059af
Vinland-Lucas <lucas.hoang@le-campus-numerique.fr>
2024-03-03 20:16:51 +0100

    UPDT Footer : Changing <ul> font size to 'text-sm' & Changing <li> line-height to 'leading-relaxed'

--- CraftedBy--Vue/src/components/Footer.vue
+++ CraftedBy--Vue/src/components/Footer.vue
@@ -11,15 +11,15 @@
 
         <div class="w-screen border-b-2 py-5">
             <a href="/" class="link link-hover pl-6">LEARN</a>
-            <div class="text-base pl-6 pt-3">
+            <div class="text-sm pl-6 pt-3">
                 <ul>
-                    <li>
+                    <li class="leading-relaxed">
                         <a href="/" class="link link-hover">Our Story</a>
                     </li>
-                    <li>
+                    <li class="leading-relaxed">
                         <a href="/" class="link link-hover">Our Craftmans</a>
                     </li>
-                    <li>
+                    <li class="leading-relaxed">
                         <a href="/" class="link link-hover">Partners</a>
                     </li>
                 </ul>
@@ -28,15 +28,15 @@
 
         <div class="w-screen border-b-2 py-5">
             <a href="/" class="link link-hover pl-6">HELP</a>
-            <div class="text-base pl-6 pt-3">
+            <div class="text-sm pl-6 pt-3">
                 <ul>
-                    <li>
+                    <li class="leading-relaxed">
                         <a href="/" class="link link-hover">Account</a>
                     </li>
-                    <li>
+                    <li class="leading-relaxed">
                         <a href="/" class="link link-hover">Wholesale</a>
                     </li>
-                    <li>
+                    <li class="leading-relaxed">
                         <a href="/" class="link link-hover">FAQs</a>
                     </li>
                 </ul>
@@ -45,15 +45,15 @@
 
         <div class="w-screen border-b-2 py-5">
             <a href="/" class="link link-hover pl-6">FOLLOW</a>
-            <div class="text-base pl-6 pt-3">
+            <div class="text-sm pl-6 pt-3">
                 <ul>
-                    <li>
+                    <li class="leading-relaxed">
                         <a href="/" class="link link-hover">Instagram</a>
                     </li>
-                    <li>
+                    <li class="leading-relaxed">
                         <a href="/" class="link link-hover">Twitter</a>
                     </li>
-                    <li>
+                    <li class="leading-relaxed">
                         <a href="/" class="link link-hover">TikTok</a>
                     </li>
                 </ul>
@@ -62,15 +62,15 @@
 
         <div class="w-screen py-5">
             <a href="/" class="link link-hover pl-6">LEGAL</a>
-            <div class="text-base pl-6 pt-3">
+            <div class="text-sm pl-6 pt-3">
                 <ul>
-                    <li>
+                    <li class="leading-relaxed">
                         <a href="/" class="link link-hover">Terms of use</a>
                     </li>
-                    <li>
+                    <li class="leading-relaxed">
                         <a href="/" class="link link-hover">Privacy policy</a>
                     </li>
-                    <li>
+                    <li class="leading-relaxed">
                         <a href="/" class="link link-hover">Cookie policy</a>
                     </li>
                 </ul>
