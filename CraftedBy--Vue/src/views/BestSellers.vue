e2c8edca339aea587ac4ff677bedd5cd3630002e
Vinland-Lucas <lucas.hoang@le-campus-numerique.fr>
2024-03-03 22:38:46 +0100

    UPDT BestSellers view : adding all 'product card' and 'py-8' to the view

--- CraftedBy--Vue/src/views/BestSellers.vue
+++ CraftedBy--Vue/src/views/BestSellers.vue
@@ -9,15 +9,28 @@
 </script>
 
 <template>
-    <div class="best--sellers w-screen h-screen bg-timberwolf px-27px">
-        <div class="font-ryman-eco text-3xl text-black text-center mb-38px pt-8">
+    <div class="best--sellers w-screen h-auto bg-timberwolf px-27px py-8">
+        <div class="font-ryman-eco text-3xl text-black text-center mb-38px">
             <h2>BEST SELLERS</h2>
         </div>
 
         <div class="grid grid-cols-2 grid-rows-2 gap-4">
-            <div class="row-span-1 col-span-1">
+            <div class="row-start-1 col-span-1">
                 <ProductCard />
             </div>
+
+            <div class="row-start-1 col-start-2 col-span-1">
+                <ProductCard />
+            </div>
+
+            <div class="row-start-2 col-span-1">
+                <ProductCard />
+            </div>
+
+            <div class="row-start-2 col-start-2 col-span-1">
+                <ProductCard />
+            </div>
+
         </div>
 
     </div>
