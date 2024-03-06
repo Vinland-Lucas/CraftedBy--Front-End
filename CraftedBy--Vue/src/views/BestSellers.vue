ac9fb933af3c998ca22c41bfbf823fa7d3d85a4d
Lucas HOANG <lucas.hoang@le-campus-numerique.fr>
2024-03-06 12:36:39 +0100

    UPDT BestSellers view : ajout de la class "text-black" à la div mère de la vue

--- CraftedBy--Vue/src/views/BestSellers.vue
+++ CraftedBy--Vue/src/views/BestSellers.vue
@@ -9,8 +9,8 @@
 </script>
 
 <template>
-    <div class="best--sellers w-screen h-auto bg-timberwolf px-27px">
-        <div class="font-ryman-eco text-3xl text-black text-center mb-38px pt-[46px]">
+    <div class="best--sellers w-screen h-auto bg-timberwolf px-27px text-black">
+        <div class="font-ryman-eco text-3xl text-center mb-38px pt-[46px]">
             <h2>BEST SELLERS</h2>
         </div>
 
