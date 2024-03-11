69dc9f6bfe72da23b4ef048c43a47f9af1582ff3
Vinland-Lucas <lucas.hoang@le-campus-numerique.fr>
2024-03-11 23:06:27 +0100

    ADD ProductPage view (not finished yet)

---
+++ CraftedBy--Vue/src/views/ProductPage.vue
@@ -0,0 +1,40 @@
+<script>
+
+</script>
+
+<template>
+    <main>
+        <div id="productpage" class="main--container bg-timberwolf text-black">
+            <div class="w-screen h-96">
+                <img 
+                    src="../../public/pexels-aidan-nguyen-2735644.webp" alt="Crafted Knife"
+                    style="width: 100%; height: 100%; object-fit: cover; object-position: center;"
+                />
+            </div>
+
+            <div class="w-screen h-auto font-dm-sans-extralight px-[30px]">
+                <h1 class="font-ryman-eco text-3xl text-center py-8">Valorant Knife de Zinzin #Slash</h1>
+
+                <p class="text-sm text-justify pb-8">
+                    Explorez l'artisanat de qualité avec notre couteau haut de gamme. Fabriqué avec précision par des artisans experts, il allie performance et durabilité pour des découpes impeccables à chaque utilisation.
+                </p>
+
+
+                <div class="h-auto">
+                    <div class="w-1/3">
+                        <h2 class="font-ryman-eco text-lg text-center">Color</h2>
+
+                        <div class="flex flex-wrap justify-center gap-1.5">
+                            <button type="button" class="w-7 h-7 bg-yellow-950 border-[0.5px] border-white"></button>
+                            <button type="button" class="w-7 h-7 bg-orange-100 border-[0.5px] border-white"></button>
+                            <button type="button" class="w-7 h-7 bg-slate-100 border-[0.5px] border-white"></button>
+                            <button type="button" class="w-7 h-7 bg-rose-200 border-[0.5px] border-white"></button>
+                        </div>
+                    </div>
+
+                    <div class=""></div>
+                </div>
+            </div>
+        </div>
+    </main>
+</template>
