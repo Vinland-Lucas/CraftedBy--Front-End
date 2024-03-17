91e4ad567d23caa05534ad009b106c30147a5767
Vinland-Lucas <lucas.hoang@le-campus-numerique.fr>
2024-03-17 23:17:37 +0100

    ADD Login page (not finished lol)

---
+++ CraftedBy--Vue/src/views/Login.vue
@@ -0,0 +1,17 @@
+<script>
+
+</script>
+
+<template>
+    <main>
+        <div id="login" class="main--container w-screen h-screen bg-timberwolf text-black flex justify-center items-center">
+            <div class="w-5/6 h-3/5 font-dm-sans-extralight ">
+                <h1 class="font-ryman-eco text-5xl">Login</h1>
+
+                <form action="" method="POST">
+                    <label for=""></label>
+                </form>
+            </div>
+        </div>
+    </main>
+</template>
