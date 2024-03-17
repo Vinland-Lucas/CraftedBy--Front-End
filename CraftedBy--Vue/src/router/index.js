14b6cceb2c70fdad59f502b5ee8eff7248fbe73b
Vinland-Lucas <lucas.hoang@le-campus-numerique.fr>
2024-03-17 23:16:18 +0100

    UPDT Router : add "Login Page" route

--- CraftedBy--Vue/src/router/index.js
+++ CraftedBy--Vue/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import Shop from '../views/Shop.vue'
 import ProductPage from '../views/ProductPage.vue'
+import Login from '../views/Login.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,6 +21,11 @@ const router = createRouter({
       path: '/productpage',
       name: 'productpage',
       component: ProductPage
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
     },
     {
       path: '/about',
